c05714ab84ab9e384d46f1e4dc0d8033e7acf7ae
Aryan kamboj <aryankammboz@email.com>
2024-04-15 21:58:06 +0530

    kl

--- src/App.js
+++ src/App.js
@@ -10,11 +10,8 @@ function App() {
     <div className="bg-[#04081a]">
       <NavBar/>
       <Hero/>
-<<<<<<< HEAD
       <WhyPart/>
-=======
-      <Form/>
->>>>>>> 6be6990bd3e1ed5e1bc5b70076a86947006a1aaf
+      <Form />
       <Footer/>
     </div>
   );
