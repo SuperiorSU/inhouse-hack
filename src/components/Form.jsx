a0383de7ad67e336ce940c137ad54c7aba048a3f
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-04-16 01:36:07 +0530

    done

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -35,11 +35,11 @@ const Form = () => {
   console.log(form);
   const members = numberofmembers(parseInt(form.members) - 1 , changeHandler);
   return (
-    <div className="flex justify-center items-center mb-10 py-7 gap-5 bg-gradient-to-b from-[#231451] to-[#04081a]">
+    <div className="sm:flex justify-center items-center mb-10 py-7 gap-5 bg-gradient-to-b from-[#231451] to-[#04081a] overflow-hidden">
       <div className="">
         <h2 className="text-6xl text-gray-400 font-bold p-3">Still Not<br/>Registered?</h2>
       </div>
-      <div className=" bg-white/5 backdrop-blur-md py-5 h-[max-content] w-[50%] rounded-md right-0 px-5 border-2 border-gray-600">
+      <div className=" bg-white/5 backdrop-blur-md my-5 py-5 sm:py-0 sm:my-0 h-[max-content] w-[100%] sm:w-[50%] rounded-md right-0 px-5 border-2 border-gray-600">
         <form>
           <input
             type="text"
--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,57 +1,45 @@
-import React from 'react';
-import './about.css'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faWindows, faAndroid, faApple } from '@fortawesome/free-brands-svg-icons';
-import { SiTryhackme } from "react-icons/si";
-const About = () => {
+import React from "react";
+import "./about.css"; // Assuming you have the CSS file imported
+
+const GlowingCard = () => {
   return (
-    <div className="container mx-auto">
-      <div className="card">
-        <div className="face face1">
-          <div className="content">
-          <SiTryhackme size={30} className='text-white inline-block me-1 ' />
-            <h3 className="text-xl font-bold">Windows</h3>
-          </div>
-        </div>
-        <div className="face face2">
-          <div className="content">
-            <p className="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
-            <a href="#" className="btn btn-primary">Read More</a>
-          </div>
-        </div>
+    <section className="my-5 py-5 overflow-hidden">
+      <div className="flex flex-col items-center ">
+        <h2 className=" lg:text-6xl md:text-6xl font-serif sm:text-6xl text-5xl font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text text-center py-3 uppercase">
+          About Our Event
+        </h2>
+        <p className="text-gray-400 text-center p-1 w-[80%]">
+          Our in-house hackathon is your chance to showcase skills, tackle
+          real-world issues, and innovate within a limited time frame. It's not
+          just about competition, but about learning, experimenting, and making
+          a difference. Join us to unleash your potential!
+        </p>
       </div>
 
-      <div className="card">
-        <div className="face face1">
-          <div className="content">
-          <SiTryhackme size={30} className='text-white inline-block me-1 ' />
-            <h3 className="text-xl font-bold">Android</h3>
+      <div className="flex flex-col lg:flex-row items-center justify-center mx-auto my-10 gap-x-7 gap-y-7 lg:gap-y-0 py-10 ">
+        <div className="container w-[20rem] h-[22rem] sm:w-[35rem] sm:h-[28rem]">
+          <div className="gradient flex flex-col justify-evenly pb-3">
+            <h2 className="text-2xl text-white  text-center pb-2  pt-2 uppercase">Judging Criteria:</h2>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">1. Innovation: </bold>How original and creative is the project idea? Does it solve a real-world problem in a unique way?</p>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">2. Functionality: </bold>Does the project work as intended? Is it user-friendly and intuitive?</p>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">3. Technical Complexity:  </bold>How technically challenging is the project? Are advanced techniques or technologies utilized?</p>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">4. Implementation:  </bold> How well is the project executed? Is the code clean, well-organized, and documented?</p>
+          <p className="px-5 text-gray-500 text-[11px]   md:text-[15px]"><bold className="text-bold text-white">5. Presentation: </bold>How effectively is the project presented? Is the demo clear, concise, and engaging?</p>
           </div>
         </div>
-        <div className="face face2">
-          <div className="content">
-            <p className="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
-            <a href="#" className="btn btn-primary">Read More</a>
-          </div>
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="face face1">
-          <div className="content">
-          <SiTryhackme size={30} className='text-white inline-block me-1 ' />
-            <h3 className="text-xl font-bold">Apple</h3>
-          </div>
-        </div>
-        <div className="face face2">
-          <div className="content">
-            <p className="text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab repudiandae, explicabo voluptate et hic cum ratione a. Officia delectus illum perferendis maiores quia molestias vitae fugiat aspernatur alias corporis?</p>
-            <a href="#" className="btn btn-primary">Read More</a>
+        <div className="container w-[20rem] h-[22rem] sm:w-[35rem] sm:h-[28rem]">
+          <div className="gradient flex flex-col justify-evenly pb-3">
+            <h2 className="text-2xl text-white  text-center pb-2  pt-2 uppercase">Rules and Regulations:</h2>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">1. Team Composition: </bold>Each team must consist of 2-4 members.</p>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">2. Project Requirements: </bold> Projects must be developed entirely during the hackathon. While open-source resources are encouraged, proper acknowledgment is mandatory.</p>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">3.  Participants</bold> must adhere to a code promoting respectful and inclusive behavior. Any form of harassment or discrimination will result in immediate disqualification.</p>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">4. Time Limit:   </bold> Participants will have to work on their projects. Late submissions will not be accepted.</p>
+          <p className="px-5 text-gray-500 text-[11px]    md:text-[15px]"><bold className="text-bold text-white">5. Intellectual Property: </bold> Participants retain full ownership of their projects. However, by participating, they grant the right to showcase their work for promotional purposes.</p>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
-export default About;
+export default GlowingCard;
