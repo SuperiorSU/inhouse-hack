3dd83ed805d12f9e5f77b5adf770b24cada8ba93
Mukultiwari03 <mukultiwari2032004@gmail.com>
2024-04-15 23:13:24 +0530

    123

--- src/App.js
+++ src/App.js
@@ -11,15 +11,10 @@ function App() {
       <NavBar/>
       <Hero/>
       <WhyPart/>
-<<<<<<< HEAD
       <About/>
-=======
-<<<<<<< HEAD
+
       <Form/>
-=======
->>>>>>> 5a2eb721beeeecc7cdcd687c49f2a8a6073a7ccf
-      <Form />
->>>>>>> c9b5e57099f145579c76300365f0ff47914ccd98
+
       <Footer/>
     </div>
   );
